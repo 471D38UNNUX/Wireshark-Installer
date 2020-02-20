18640b78bdfb28beb6eb19b160b62903fe4470b6
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-02-20 13:01:58 +0700

    Update Wireshark Installer

--- Wireshark-Installer-For-Termux.sh
+++ Wireshark-Installer-For-Termux.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 apt update & apt upgrade -y
-apt install wireshark-gtk -y
+apt install wireshark-gtk tsu -y
 
 echo "You can now launch Wireshark."
 echo "Run wireshark-gtk for start Wireshark GUI or tshark -h for Wireshark CLI."
