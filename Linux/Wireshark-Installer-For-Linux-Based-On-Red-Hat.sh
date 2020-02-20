e6e98c479470f16a607f7b629b90aa07bfda6387
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-02-20 22:42:28 +0700

    Update Wireshark Installer

--- Linux/Wireshark-Installer-For-Linux-Based-On-Red-Hat.sh
+++ Linux/Wireshark-Installer-For-Linux-Based-On-Red-Hat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo yum update -y
-sudo yum install wireshark -y
+sudo yum install '*wireshark*' -y
 
 echo "You can now launch Wireshark."
 echo "Run wireshark for start Wireshark GUI or tshark -h for Wireshark CLI."
