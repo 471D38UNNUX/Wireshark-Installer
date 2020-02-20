2c9e7732ec45e7358768e58f5856bd970627c478
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2020-02-20 22:38:36 +0700

    Update Wireshark Installer

--- Linux/Wireshark-Installer-For-Linux-Based-On-Arch.sh
+++ Linux/Wireshark-Installer-For-Linux-Based-On-Arch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -S wireshark-qt
+sudo pacman -S wireshark-qt wireshark-cli
 
 echo "You can now launch Wireshark."
 echo "Run wireshark for start Wireshark GUI or tshark -h for Wireshark CLI."
